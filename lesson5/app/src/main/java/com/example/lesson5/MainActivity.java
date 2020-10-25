1f6ddda42ba8a83154d9f28794f19e64f18623ef
kbc18a11 <wada121400@gmail.com>
2020-10-25 16:59:57 +0900

    5.2まで終わった

---
+++ lesson5/app/src/main/java/com/example/lesson5/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.lesson5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> menuList = new ArrayList<>();
+        menuList.add("ちきん");
+        menuList.add("うし");
+        menuList.add("ぶた");
+        menuList.add("やさい");
+
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                menuList
+        );
+
+        lvMenu.setAdapter(adapter);
+    }
+
+
+}
