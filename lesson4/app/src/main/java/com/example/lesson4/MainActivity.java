bba04d0dbd5554dea24dd056f48e8467917b399a
kbc18a11 <wada121400@gmail.com>
2020-10-25 16:11:32 +0900

    lesson4終了

--- lesson4/app/src/main/java/com/example/lesson4/MainActivity.java
+++ lesson4/app/src/main/java/com/example/lesson4/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.lesson4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btClick = findViewById(R.id.btClick);
+        btClick.setOnClickListener(new HelloListener());
+
+        Button btClear = findViewById(R.id.btClear);
+        btClear.setOnClickListener(new ClearListener());
+    }
+
+    private class HelloListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            EditText input = findViewById(R.id.etName);
+            TextView output = findViewById(R.id.tvOutput);
+            String inputStr = input.getText().toString();
+            output.setText(inputStr + "殺す");
+        }
+    }
+
+    private class ClearListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            EditText input = findViewById(R.id.etName);
+            TextView output = findViewById(R.id.tvOutput);
+            input.setText("");
+            output.setText("");
+        }
     }
 }
